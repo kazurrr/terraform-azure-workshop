cc555c40d71eac85798f93830846e3b76df4e4c4
Karol Masuhr <kmasuhr@pgs-soft.com>
2018-08-13 15:20:17 +0200

    Add workshop with azure function

---
+++ w2/app.tf
@@ -0,0 +1,35 @@
+resource "azurerm_resource_group" "test" {
+  name     = "azure-functions-test-rg"
+  location = "westus2"
+}
+
+resource "azurerm_storage_account" "test" {
+  name                     = "functionsapptes345"
+  resource_group_name      = "${azurerm_resource_group.test.name}"
+  location                 = "${azurerm_resource_group.test.location}"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "test" {
+  name                = "azure-functions-test-service-plan"
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_function_app" "test" {
+  name                      = "kmasuhr-test-azure-functions"
+  location                  = "${azurerm_resource_group.test.location}"
+  resource_group_name       = "${azurerm_resource_group.test.name}"
+  app_service_plan_id       = "${azurerm_app_service_plan.test.id}"
+  storage_connection_string = "${azurerm_storage_account.test.primary_connection_string}"
+}
+
+output "function_url" {
+  value = "https://${azurerm_function_app.test.default_hostname}"
+}

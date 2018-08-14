0b5cb1563ba97643435133b97e17b5abfc401c10
Karol Masuhr <kmasuhr@pgs-soft.com>
2018-08-14 14:27:30 +0200

    Add workshop #2

--- w2/main.tf
+++ w2/main.tf
@@ -1,35 +1,22 @@
+variable "prefix" {
+  type        = "string"
+  description = "Pierwsza litera imienia i nazwisko pisane łącznie."
+}
+
+variable "location" {
+  type    = "string"
+  default = "westus"
+}
+
 resource "azurerm_resource_group" "test" {
-  name     = "azure-functions-test-rg"
-  location = "westus2"
+  name     = "${vat.prefix}-functionapp-rg"
+  location = "${var.location}"
 }
 
 resource "azurerm_storage_account" "test" {
-  name                     = "functionsapptes345"
+  name                     = "${var.prefix}functionapp"
   resource_group_name      = "${azurerm_resource_group.test.name}"
   location                 = "${azurerm_resource_group.test.location}"
   account_tier             = "Standard"
   account_replication_type = "LRS"
-}
-
-resource "azurerm_app_service_plan" "test" {
-  name                = "azure-functions-test-service-plan"
-  location            = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
-}
-
-resource "azurerm_function_app" "test" {
-  name                      = "kmasuhr-test-azure-functions"
-  location                  = "${azurerm_resource_group.test.location}"
-  resource_group_name       = "${azurerm_resource_group.test.name}"
-  app_service_plan_id       = "${azurerm_app_service_plan.test.id}"
-  storage_connection_string = "${azurerm_storage_account.test.primary_connection_string}"
-}
-
-output "function_url" {
-  value = "https://${azurerm_function_app.test.default_hostname}"
 }

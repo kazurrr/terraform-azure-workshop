fa891bc02ceb2bb274b2b4e9cf6b914c9a542638
Karol Masuhr <kmasuhr@pgs-soft.com>
2018-08-16 11:49:29 +0200

    Update nodejs app

--- w3/function-app-hello-world/index.js
+++ w3/function-app-hello-world/index.js
@@ -2,7 +2,13 @@ module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     context.res = {
-        body: "Hello world"
+        body: {
+            message: "Hello world!",
+            envName: process.env.NAME,
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        }
     };
     context.done();
 };
